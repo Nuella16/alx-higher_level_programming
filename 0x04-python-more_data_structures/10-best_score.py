2c345c4d4885c238ae6d1bbbc01dd26e5f5f3489
Nuella16 <nanaessilfua18@gmail.com.com>
2023-09-21 11:15:06 +0000

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
 
+
 def best_score(a_dictionary):
     """
-    A function that returns a key with the biggest integer value in a dictionary.
-    
-    Args:
-    a_dictionary (dict): The input dictionary.
-
-    Returns:
-    str or None: The key with the maximum integer value, or None if the dictionary is empty.
+    A function that returns a key with the biggest integer value.
     """
-    if not a_dictionary:  # Check if the dictionary is empty
-        return None
-
-    # Use the max function with a lambda function as the key to find the key with the maximum value
-    leader = max(a_dictionary, key=lambda k: a_dictionary[k])
-    return leader
-
+    if a_dictionary:
+        my_list = list(a_dictionary.keys())
+        score = 0
+        leader = ""
+        for i in my_list:
+            if a_dictionary[i] > score:
+                score = a_dictionary[i]
+                leader = i
+        return leader
